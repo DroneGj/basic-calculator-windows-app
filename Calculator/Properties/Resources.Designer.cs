99448fc482ad11c8c0609bdb2a259cf0f90691db
DroneGj <52096917+DroneGj@users.noreply.github.com>
2020-01-31 10:35:10 +0530

    First Commit

--- Calculator/Properties/Resources.Designer.cs
+++ Calculator/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Calculator.Properties {
+namespace calculatorBasicUI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Calculator.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Calculator.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("calculatorBasicUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
